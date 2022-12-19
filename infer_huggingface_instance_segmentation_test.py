984927868bd1ceb589542e99b24605516d83b003
Allan <allan.kouidri@gmail.com>
2022-12-19 11:27:21 +0100

    Edit param for test

--- infer_huggingface_instance_segmentation_test.py
+++ infer_huggingface_instance_segmentation_test.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 def test(t, data_dict):
     logger.info("===== Test::infer Hugging Face instance segmentation=====")
     params = task.get_parameters(t)
-    params["model_name_or_path"] = "facebook/maskformer-swin-tiny-ade"
+    params["model_name"] = "facebook/maskformer-swin-tiny-ade"
     img = cv2.imread(data_dict["images"]["detection"]["coco"])[::-1]
     input_img_0 = t.getInput(0)
     input_img_0.setImage(img)
